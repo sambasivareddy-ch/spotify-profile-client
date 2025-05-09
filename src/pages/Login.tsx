cc694e3fc2e5c1d174758890d279429b91eaebb9
V N G Samba Siva Reddy Chinta <151616423+sambasivareddy-ch@users.noreply.github.com>
2025-05-09 20:56:29 +0530

    Update Login.tsx with correct CSS file

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 import Button from "../components/Button";
-import styles from "../styles/Login.module.css";
+import styles from "../styles/login.module.css";
 
 const Login: React.FC = () => {
     const loginClickHandler = async () => {
