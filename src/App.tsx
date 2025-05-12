60aef7f92ed4c652907fcf9870077a04c49aa487
V N G Samba Siva Reddy Chinta <151616423+sambasivareddy-ch@users.noreply.github.com>
2025-05-12 18:45:59 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import { useEffect } from "react"
-import { useNavigate } from "react-router-dom"
+// import { useEffect } from "react"
+// import { useNavigate } from "react-router-dom"
 
 import { Route, Routes } from "react-router-dom"
 import Login from "./pages/Login"
@@ -11,7 +11,7 @@ import Song from "./pages/Song"
 import PlaylistSongs from "./pages/PlaylistSongs"
 
 function App() {
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
 
   // useEffect(() => {
   //   const checkLoginStatus = async () => {
